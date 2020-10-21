e0c2773168984df4f6b2e777098faa52e0ada32e
stygiang <dscgl727@gmail.com>
2020-10-21 11:42:53 -0400

    none

---
+++ script.js
@@ -0,0 +1,54 @@
+//card 1
+document.querySelector('.select-ocean').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card').style.display = 'grid';
+})
+document.querySelector('.select-nature').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
+document.querySelector('.select-city').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card-3').style.display = 'grid';
+})
+document.querySelector('.select-haven').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
+// card 2
+document.querySelector('.select-ocean-2').addEventListener('click', function(){
+    document.querySelector('.card-2').style.display = 'none';
+    document.querySelector('.card').style.display = 'grid';
+    
+})
+document.querySelector('.select-nature-2').addEventListener('click', function(){
+    document.querySelector('.card-2').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
+document.querySelector('.select-city-2').addEventListener('click', function(){
+    document.querySelector('.card-2').style.display = 'none';
+    document.querySelector('.card-3').style.display = 'grid';
+    
+})
+document.querySelector('.select-haven-2').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
+// card 3
+document.querySelector('.select-ocean-3').addEventListener('click', function(){
+    document.querySelector('.card-3').style.display = 'none';
+    document.querySelector('.card').style.display = 'grid';
+    
+})
+document.querySelector('.select-nature-3').addEventListener('click', function(){
+    document.querySelector('.card-3').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
+document.querySelector('.select-city-3').addEventListener('click', function(){
+    document.querySelector('.card-3').style.display = 'none';
+    document.querySelector('.card-3').style.display = 'grid';
+})
+document.querySelector('.select-haven-3').addEventListener('click', function(){
+    document.querySelector('.card').style.display = 'none';
+    document.querySelector('.card-2').style.display = 'grid';
+})
